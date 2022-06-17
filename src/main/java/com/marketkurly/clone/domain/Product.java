6451a388733da1ca95e4c44784b88853565378da
printf-jin <dlghwls7662@naver.com>
2022-06-17 19:53:46 +0900

    FEAT: Product 엔티티 멤버 변수 추가

--- src/main/java/com/marketkurly/clone/domain/Product.java
+++ src/main/java/com/marketkurly/clone/domain/Product.java
@@ -1,8 +1,48 @@
 package com.marketkurly.clone.domain;
 
-import javax.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Getter
 @Entity
 public class Product extends TimeStamp{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private String category;
+
+    private String subtitle;
+
+    private String salesUnit;
+
+    private String weight;
+
+    private String shippingCategory;
+
+    private String orgin;
+
+    private String packagingType;
+
+    private String allergic;
+
+    private String info;
+
+    private String thumbnailUrl;
+
+    private String contentImageUrl;
+    
 
 }
