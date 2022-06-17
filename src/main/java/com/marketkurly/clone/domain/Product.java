677fe3e2f298e2a3abfe0994d26929ad7e112beb
printf-jin <dlghwls7662@naver.com>
2022-06-18 00:07:16 +0900

    FEAT: Product엔티티 클래스 생성 및 연관관계 설정

--- src/main/java/com/marketkurly/clone/domain/Product.java
+++ src/main/java/com/marketkurly/clone/domain/Product.java
@@ -1,8 +1,31 @@
 package com.marketkurly.clone.domain;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
 @Entity
 public class Product extends TimeStamp{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private String category;
+    private String subtitle;
+    private String salesUnit;
+    private String weight;
+    private String shippingCategory;
+    private String orgin;
+    private String packagingType;
+    private String allergic;
+    private String info;
+    private String thumbnail;
+    private String contentImageUrl;
 }
