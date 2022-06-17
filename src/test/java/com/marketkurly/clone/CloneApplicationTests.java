4e93832ca555842443eacc8b94ec9dcd41086a20
printf-jin <dlghwls7662@naver.com>
2022-06-17 16:25:19 +0900

    기본 설정 파일 업데이트

---
+++ src/test/java/com/marketkurly/clone/CloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketkurly.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
