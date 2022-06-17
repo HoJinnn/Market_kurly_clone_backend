a066cb51cc48ccdd6657f1d2ea71f73d88dfbfbc
printf-jin <dlghwls7662@naver.com>
2022-06-18 00:12:10 +0900

    FEAT: Orders엔티티 클래스 생성 및 연관관계 설정

---
+++ src/main/java/com/marketkurly/clone/domain/Orders.java
@@ -0,0 +1,27 @@
+package com.marketkurly.clone.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Orders extends TimeStamp{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orders_id")
+    private Long id;
+
+    private int payment;
+
+    @OneToMany(mappedBy = "orders")
+    private List<Cart> carts;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+}
