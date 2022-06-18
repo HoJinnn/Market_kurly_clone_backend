e67f10b90114b56aeb36b7f91a4cd5f6e8c78bc8
printf-jin <dlghwls7662@naver.com>
2022-06-18 17:14:00 +0900

    FEAT: CartResponseDto생성

---
+++ src/main/java/com/marketkurly/clone/dto/CartResponseDto.java
@@ -0,0 +1,17 @@
+package com.marketkurly.clone.dto;
+
+import com.marketkurly.clone.domain.Cart;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class CartResponseDto {
+    private String address;
+    private List<Cart> carts;
+
+    public CartResponseDto(S) {
+
+    }
+}
+
