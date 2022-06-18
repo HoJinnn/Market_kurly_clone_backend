197ee8f5405682fc813840fd048a87ad8688b2db
printf-jin <dlghwls7662@naver.com>
2022-06-19 01:39:12 +0900

    FEAT: 장바구니 데이터 저장용 API추가

--- src/main/java/com/marketkurly/clone/controller/CartController.java
+++ src/main/java/com/marketkurly/clone/controller/CartController.java
@@ -1,9 +1,12 @@
 package com.marketkurly.clone.controller;
 
+import com.marketkurly.clone.dto.CartRequestDto;
 import com.marketkurly.clone.dto.CartResponseDto;
 import com.marketkurly.clone.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -15,5 +18,11 @@ public class CartController {
     @GetMapping("/api/cart")
     public CartResponseDto getCart() {
         return cartService.getCart();
+    }
+
+    //데이터 저장 테스트용 API
+    @PostMapping("/api/cart")
+    public void saveCart(@RequestBody CartRequestDto requestDto) {
+        cartService.setData(requestDto);
     }
 }
