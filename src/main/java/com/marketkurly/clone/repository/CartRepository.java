b8a8e1f4d04edbd268b6ae262b06609bc85399b6
printf-jin <dlghwls7662@naver.com>
2022-06-18 17:14:22 +0900

    FEAT: CartRepository 생성

---
+++ src/main/java/com/marketkurly/clone/repository/CartRepository.java
@@ -0,0 +1,18 @@
+package com.marketkurly.clone.repository;
+
+import com.marketkurly.clone.domain.Cart;
+import com.marketkurly.clone.dto.CartResponseDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+//    @Query("select m from User m where m.usernaem = :username and m.age = :age")
+//    List<User> findUser(@Param("username") String username, @Param("age") int age);
+//
+//    @Query("select new com.marketkurly.clone.dto.CartResponseDto(c.quantity, c.totalPrice) from Cart c join c.user u")
+//    List<CartResponseDto> findCart();
+}
