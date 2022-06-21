cf496d59e3282bc360b9eadb253d65e1e0b2b7fa
printf-jin <dlghwls7662@naver.com>
2022-06-21 14:49:35 +0900

    FEAT: 게시물 전제조회 기능 추가

--- src/main/java/com/marketkurly/clone/controller/ProductController.java
+++ src/main/java/com/marketkurly/clone/controller/ProductController.java
@@ -2,18 +2,21 @@ package com.marketkurly.clone.controller;
 
 import com.marketkurly.clone.domain.Product;
 import com.marketkurly.clone.dto.ProductRequestDto;
+import com.marketkurly.clone.dto.ProductResponseDto;
 import com.marketkurly.clone.repository.ProductRepository;
+import com.marketkurly.clone.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class ProductController {
 
     private final ProductRepository productRepository;
+    private final ProductService productService;
 
     @PostMapping("/api/product")
     public ResponseEntity<String> addProduct(@RequestBody ProductRequestDto requestDto) {
@@ -34,5 +37,11 @@ public class ProductController {
                 .build();
         productRepository.save(product);
         return ResponseEntity.ok("상품 등록 성공");
+    }
+
+    //게시물 전체 조회
+    @GetMapping("/api/products")
+    public List<ProductResponseDto> getproducts() {
+        return productService.getProducts();
     }
 }
