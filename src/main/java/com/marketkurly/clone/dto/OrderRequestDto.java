0f6580e796a0318dd2af721cca6b3b81c5c899dd
ParkSungGyu1 <steven0507@naver.com>
2022-06-22 18:02:23 +0900

    order test

--- src/main/java/com/marketkurly/clone/dto/OrderRequestDto.java
+++ src/main/java/com/marketkurly/clone/dto/OrderRequestDto.java
@@ -1,10 +1,13 @@
 package com.marketkurly.clone.dto;
 
+import com.marketkurly.clone.domain.Cart;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class OrderRequestDto {
     private String address;
-    private List<cart> product;
+    private List<Cart> product;
     private int payment;
 }
